5dc89433f0dbaaff036fc9748ae2568f5f10713a
brajeshmeena17 <brajeshmeena302@gmail.com>
2025-12-24 14:30:39 +0530

    learnibng js

--- learn.js
+++ learn.js
@@ -1,3 +1,11 @@
-let a=4;
-let b =5
-print(a+b);
+var a = 4;
+var b = 5
+const name="Raja";
+let temp=28.2;
+temp=30.2;
+a="gandu";
+let v=[1,2,3,4];
+console.log(a);
+console.log(name);
+console.log(temp);
+console.log(v);
---
+++ raja.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // variable / data types
+    int age=22;
+    string name="Raja";
+    double temp=28.2;
+    vector<int>v={1,2,3,4};
+
+    cout<<age<<endl;
+    cout<<name<<endl;
+    cout<<temp<<endl;
+    for(auto xd:v)cout<<xd<<" ";
+    cout<<endl;
+
+}
